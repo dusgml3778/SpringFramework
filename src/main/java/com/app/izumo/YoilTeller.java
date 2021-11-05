0320c3d50e627df5a021d23c53968dbc08e029b4
kimmingye <dusgml3779@gmail.com>
2021-11-05 11:27:24 +0900

    Mvc Pattern

--- src/main/java/com/app/izumo/YoilTeller.java
+++ src/main/java/com/app/izumo/YoilTeller.java
@@ -16,6 +16,7 @@ public class YoilTeller {
 	@RequestMapping("/getYoil")
 	public void main(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
+		// 1.입력
 		String year = request.getParameter("year");
 		String month = request.getParameter("month");
 		String day = request.getParameter("day");
@@ -24,15 +25,16 @@ public class YoilTeller {
 		int mm = Integer.parseInt(month);
 		int dd = Integer.parseInt(day);
 
+		// 2.처리
 		Calendar cal = Calendar.getInstance();
 		cal.set(yyyy, mm - 1, dd);
 
 		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK); // 1.Sunday 2. Monday
-
 		char yoil = " 일월화수목금토".charAt(dayOfWeek);
 
-		response.setContentType("text/html");
-		response.setCharacterEncoding("utf-8");
+		// 3.출력
+		response.setContentType("text/html"); // 응답의 형식을 html로 지정
+		response.setCharacterEncoding("utf-8"); // 응답의 인코딩을 utf-8로 지정
 		PrintWriter out = response.getWriter(); // reponse 객체에서 브라우저로의 출력 스트림을 얻는다
 		out.println(year + " year " + month + " month " + day + " day ");
 		out.println(yoil);
---
+++ src/main/webapp/WEB-INF/views/yoil.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+	pageEncoding="utf-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<p>${year}年 ${month}月 ${day}日は ${yoil}曜日です</p>
+
+</body>
+</html>
